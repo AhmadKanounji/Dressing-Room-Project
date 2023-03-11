a887c0dc0972fff989d529c267b048b887202cc9
Serena Kobeissi <serenakobeissi@gmail.com>
2023-03-11 19:29:34 +0200

    Login Page Commit

--- Dressing Room/MauiProgram.cs
+++ Dressing Room/MauiProgram.cs
@@ -13,6 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("OPTIMA_B.TTF", "OPTIMA_B");
+				fonts.AddFont("Quora.ttf", "Quora");
 			});
 
 #if DEBUG
